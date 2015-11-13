b650bfe46de0c7ad940285aaa0f94a0a0a4df5c3
Richard Cojoc <richard@192-168-0-100.rdsnet.ro>
2015-11-13 02:39:43 +0200

    first

---
+++ app/components/Notes.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Note from './Note.jsx';
+
+export default class Notes extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.renderNote = this.renderNote.bind(this);
+  }
+
+  render() {
+    const notes = this.props.items;
+
+    return <ul className="notes">{notes.map(this.renderNote)}</ul>
+  }
+
+  renderNote(note) {
+    return (
+      <li className="note" key={note.id}>
+        <Note task={note.task} onDelete={this.props.onDelete.bind(null, note.id)} onEdit={this.props.onEdit.bind(null, note.id)} />
+      </li>
+    );
+  }
+}
